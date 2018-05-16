a97247126f9c761bed1623ba735931d3a4b2ad89
Julien CASTILLEJOS <julien.castillejos@epitech.eu>
2018-05-16 11:28:35 +0200

    Add my_strjoin to libmy

---
+++ lib/str/my_strjoin.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2018
+** my_strjoin
+** File description:
+** Concatenate two strings
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+char	*my_strjoin(char const *str1, char const *str2)
+{
+	char	*str = NULL;
+	int	len = 0;
+
+	len = strlen(str1) + strlen(str2);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL) {
+		return (NULL);
+	}
+	strcpy(str, str1);
+	strcat(str, str2);
+	return (str);
+}
