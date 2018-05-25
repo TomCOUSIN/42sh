7c415d743a8b154f14d2c0c1b47d5ac4ad40fa36
cousin_t <tom.cousin@epitech.eu>
2018-05-25 18:52:00 +0200

    fix segfault with history

--- src/history_handling/do_history_command.c
+++ src/history_handling/do_history_command.c
@@ -17,12 +17,12 @@ int	do_history_command(char **cmd, char ***env,
 	(void)env;
 	(void)shell;
 	if (strncmp(cmd[0], "!!", 2) == 0) {
-		*status = 0;
-		*cmd = shell->history->prev->cmd;
-		return (0);
+		//*status = 0;
+		//*cmd = shell->history->prev->cmd;
+		//return (0);
 	}
 	if (strncmp(cmd[0], "!", 1) == 0) {
-		*cmd = search_history(shell->history, my_get_nbr(cmd[0]));
+		//*cmd = search_history(shell->history, my_get_nbr(cmd[0]));
 	}
 	if (strncmp(cmd[0], "history", 7) == 0) {
 		*status = show_history(shell->history);
